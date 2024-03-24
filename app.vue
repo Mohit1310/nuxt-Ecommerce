4d9524dda5df762a416ab868fa0fd40aa715068c
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2024-03-24 12:38:08 +0530

    initial setup

---
+++ app.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="fixed -z-10 bg-[#f2f2f2] w-full h-[100vh]" />
+  <NuxtPage />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">Index Page</div>
+</template>
+
+<script setup></script>
